db9ebf7c7487da75b25323670f353711f6e00d1e
hongsujin2eeZyo <hong266770@gmail.com>
2025-11-07 03:29:20 +0900

    practice: 6주차 미션2(2)  인증

--- practice/Chapter_06/tanstack02/src/App.tsx
+++ practice/Chapter_06/tanstack02/src/App.tsx
@@ -1,13 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import InfinitePostsJsonPlaceholder from './components/InfinitePostsJsonPlacehorder';
+//import InfinitePostsJsonPlaceholder from './components/InfinitePostsJsonPlacehorder';
+import InfinitePostsAutoJsonPlaceholder from './components/InfintePostsAutoJsonPlaceholder';
+
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <InfinitePostsJsonPlaceholder />
+      <InfinitePostsAutoJsonPlaceholder />
       {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
