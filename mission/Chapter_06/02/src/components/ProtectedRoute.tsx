ba1c5a6951e2aab2820a857a4b8045e0f3ba1e10
hongsujin2eeZyo <hong266770@gmail.com>
2025-11-09 18:31:06 +0900

    fixed: ProtectedRoute 수정

--- mission/Chapter_06/02/src/components/ProtectedRoute.tsx
+++ mission/Chapter_06/02/src/components/ProtectedRoute.tsx
@@ -1,44 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { getToken, removeToken } from "../utils/storage";
-import { api } from "../api/axiosInstance";
+import { useAuthCheck } from "../hooks/useAuthCheck";
 
-type ProtectedRouteProps = {
-  children: React.ReactNode;
-};
-
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const [isValid, setIsValid] = useState<boolean | null>(null);
+const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const location = useLocation();
- 
-  useEffect(() => {
+  const { isLoading, isError } = useAuthCheck();
 
-    const token = getToken();
-
-    if (!token) {
-      setIsValid(false);
-      return;
-    }
-
-
-  
-    const validateToken = async () => {
-      try {
-
-        const res = await api.get(`${import.meta.env.VITE_SERVER_API_URL}/v1/auth/protected`);
-        console.log("토큰 검증 성공:", res.data);
-        setIsValid(true);
-      } catch (error: any) {
-        console.error("토큰 검증 실패:", error);
-        removeToken();
-        setIsValid(false);
-      }
-    };
-
-    validateToken();
-  }, []);
-
-  if (isValid === null) {
+  if (isLoading) {
     return (
       <div className="flex items-center justify-center h-screen text-gray-300">
         인증 확인 중...
@@ -46,7 +14,7 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
     );
   }
 
-  if (!isValid) {
+  if (isError) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
